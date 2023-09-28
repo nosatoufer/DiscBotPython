78b54dca60ecc7637d0a8f8de4261d170bd1e7fe
nosatoufer <gdufour05@gmail.com>
2023-09-28 15:45:58 +0200

    add health check

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 # This is for health check
 def run():
     port = int(os.environ.get('PORT') or 10000) 
-    server_address = ('', port)
+    server_address = ('0.0.0.0', port)
     httpd = http.server.HTTPServer(server_address, MyHandler)
     httpd.serve_forever()
 
@@ -20,7 +20,7 @@ def run_discord_bot():
 
 
 if __name__ == '__main__':
-
+    
     socket_server_process = multiprocessing.Process(target=run)
     discord_bot = multiprocessing.Process(target=run_discord_bot)
 
