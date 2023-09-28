062060204f0bcdbf76fc691294b67b045c0d2a4f
nosatoufer <gdufour05@gmail.com>
2023-09-28 11:03:15 +0200

    Change port to env

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import bot
 import multiprocessing
 import socket
+import os 
 
 # This is for render
 def run_socket_server():
     mytcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    mytcp.bind(('0.0.0.0', 8080))
+    mytcp.bind(('0.0.0.0', os.environ['PORT']))
     mytcp.listen(1)
     while(1):
         mytcp.listen(1)
